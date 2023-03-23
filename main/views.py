9d8cef3efb63c7caa763f3f15fa4ae5ae303ca26
lattecoff <lattecoff@lattecoff.com>
2023-03-23 19:57:56 +0300

    Added new page "About us".

--- main/views.py
+++ main/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h4>Hello</h4>")
+	return render("<h4>ДЗ по созданию нового url-адреса - выполнено</h4>")
+
+
+def about(request):
+	return HttpResponse("<h4>About</h4>")
