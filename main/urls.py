73eada8b8b9a56ea9279b232aed5d4e96b34f2f3
lattecoff <lattecoff@lattecoff.com>
2023-03-25 19:09:57 +0300

    Added fields for models.

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("about_us", views.about)
-
+    path("", views.index, name="home"),
+    path("about_us", views.about, name="about")
 ]
