f1bec72195dbcbfc1dd831279b4833587666e89d
lattecoff <lattecoff@lattecoff.com>
2023-03-30 19:59:18 +0300

    Added function for show data on website.

--- main/models.py
+++ main/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+	class Meta:
+		verbose_name = "Задача"
+		verbose_name_plural = "Задачи"
+	
 	title = models.CharField("Название", max_length=50)
 	task = models.TextField("Описание")
 
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 def index(request):
-	return render(request, "main/index.html")
+	task = Task.objects.all()
+	return render(request, "main/index.html", {"title": "Главная страница", "tasks": task})
 
 
 def about(request):
